a9af4982661c292ea19bfd94bda92c08823a0610
BHessel <hesselfitness@gmail.com>
2020-12-16 15:09:28 -0500

    first 2 deliverables done

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,10 +7,31 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushi: [],
+    position: [0, 4]
+  }
+
+  handleSushiRotation = () => {
+    console.log("Hi")
+    this.setState({
+      position: [ this.state.position[0] + 4, this.state.position[1] + 4]
+    })
+  }
+
+  async componentDidMount(){
+    const response = await fetch(API)
+    const sushi = await response.json()
+    this.setState({ sushi })
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer />
+        <SushiContainer 
+          sushi={this.state.sushi.slice(this.state.position[0], this.state.position[1])}
+          handleSushiRotation={this.handleSushiRotation}
+        />
         <Table />
       </div>
     );
